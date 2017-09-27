07e9d45d2ca23acef0dbd15ef77cce82eb242aed
Arjemariel Requina <you@example.com>
2017-09-28 00:27:42 +0800

    Added round-robin algorithm initial version

--- sub/algorithm.py
+++ sub/algorithm.py
@@ -1,19 +1,31 @@
-
+import copy
 
 class Algorithm(object):
-    def __init__(self, name=None, processes=[], awt=0, unit="ms"):
+    def __init__(self, name=None, processes=[], awt=0, unit="ms", time_slice=4):
         self.name = name
         self.processes = processes
         self.awt = awt
         self.unit = unit
         self.queue = []
+        self.time_slice = time_slice
+        self.total_burst = 0
+
+        self.get_total_burst()
         self.perform()
+
+    def get_total_burst(self):
+        for process in self.processes:
+            self.total_burst += process.burst
 
     def perform(self):
         if self.name == "FCFS":
             self.fcfs()
         elif self.name == "SJF":
             self.sjf()
+        elif self.name == "PRIORITY":
+            self.priority()
+        elif self.name == "RROBIN":
+            self.round_robin()
 
     def fcfs(self):
         self.processes = sorted(self.processes, key=lambda x: x.order, reverse=False)
@@ -43,13 +55,68 @@ class Algorithm(object):
             self.queue.append(process)
         self.awt = total_waiting / len(self.processes)
 
+    def priority(self):
+        self.processes = sorted(self.processes, key=lambda x: x.order, reverse=False)
+        self.processes = sorted(self.processes, key=lambda x: x.priority, reverse=False)
+        current_time = 0
+        total_waiting = 0
+        for process in self.processes:
+            process.start = current_time
+            process.end = current_time + process.burst
+            process.waiting = current_time
+            current_time = current_time + process.burst
+            total_waiting += process.waiting
+            self.queue.append(process)
+        self.awt = total_waiting / len(self.processes)
+
+    def round_robin(self):
+        self.processes = sorted(self.processes, key=lambda x: x.order, reverse=False)
+        tick = 0
+        idx = 0
+        current_time = 0
+        total_waiting = 0
+        not_done = len(self.processes) - 1
+        done_processes = []
+
+        while not_done:
+            process = self.processes[idx]
+            process.start = current_time
+
+            if current_time % self.time_slice != 0:
+                process.decrease_burst(factor=(current_time % self.time_slice))
+                process.end = current_time + (current_time % self.time_slice)
+                current_time += (current_time % self.time_slice)
+            else:
+                process.decreas
+
+            process.decrease_burst(factor=self.time_slice)
+            if process.counter
+        while tick <= self.total_burst:
+            process = self.processes[idx]
+            process.decrease_burst()
+            if process.is_done() or (tick > 0  and tick % self.time_slice == 0):
+                process.executions += self.time_slice
+                process.start = start
+                process.end = tick
+                self.queue.append(copy.deepcopy(process))
+                if process.is_done():
+                    process.waiting = tick - process.executions
+                    total_waiting += tick - process.executions
+
+                idx += 1
+                if idx >= len(self.processes):
+                    idx = 0
+
+            tick += 1
+        self.awt = total_waiting / len(self.processes)
+
     def get_awt(self):
         return str(self.awt) + self.unit
 
     def get_gantt(self):
         gantt = ""
         start = self.processes[0].start
-        for idx, process in enumerate(self.processes):
+        for idx, process in enumerate(self.queue):
             gantt += process.name + "(" + str(start) + "-" + str(process.end) + ")"
             if idx == len(self.processes) - 1:
                 gantt += ""
--- sub/process.py
+++ sub/process.py
@@ -1,23 +1,29 @@
 
 
 class Process(object):
-    def __init__(self, order=-1, name=None, arrival=0, burst=0, priority=0, waiting=0, unit="ms"):
+    def __init__(self, order=-1, name=None, arrival=0, burst=0, priority=0, waiting=0, unit="ms", executions=0):
         self.order = order
         self.name = name
         self.arrival = arrival
         self.burst = burst
+        self.counter = 0
         self.priority = priority
         self.waiting = waiting
         self.unit = unit
         self.start = 0
         self.end = burst
+        self.executions = executions
 
     def __str__(self):
         return str(self.name)
 
     def decrease_burst(self, factor=1):
-        if self.burst:
-            self.burst -= 1
+        if self.counter < self.burst:
+            self.counter -= factor
+
+    def is_done(self):
+        return self.counter == 0
+
 
     def get_arrival(self):
         return str(self.arrival) + self.unit
