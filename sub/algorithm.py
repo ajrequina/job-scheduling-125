21b9feb0deb69f0fbe5ac69b78ac2f2a85eea90a
Arjemariel J. Requina <rjrequina@gmail.com>
2017-10-03 18:06:30 +0800

    Removed print statements

--- sub/algorithm.py
+++ sub/algorithm.py
@@ -119,53 +119,31 @@ class Algorithm(object):
         idx = 1
         current = None
         arrived_processes = []
-        print(self.total_burst)
         while current_time <= self.total_burst:
-            print("Time: " + str(current_time))
             for item in processes:
                 if item.arrival == current_time:
                     arrived_processes.append(item)
             arrived_processes = sorted(arrived_processes, key=lambda x: x.counter, reverse=False)
-            print("Current is null? = " + str(current is None))
-            if not current:``
+            if not current:
                 current = arrived_processes.pop(0)
                 self.job_order.append([current.name, current_time, current_time + 1])
                 current.waiting = (current_time - (current.executions)) - current.arrival
                 current.executions += 1
                 current.decrease_burst(factor=1)
-                print("Process name: " + current.name)
-                print("Process executions: " + str(current.executions))
-                print("Process CPU time: " + str(current.counter))
-                print("Process Waiting: " + str(current.waiting))
             else:
-                print("Have arrived_processes? = " + str(len(arrived_processes) > 0))
                 if len(arrived_processes):
-                    print("Current burst time is zero? = " + str(current.counter == 0))
                     if current.counter == 0:
                         current = arrived_processes.pop(0)
                         self.job_order.append([current.name, current_time, current_time + 1])
                         current.waiting = (current_time - (current.executions)) - current.arrival
                         current.executions += 1
                         current.decrease_burst(factor=1)
-                        print("Process name: " + current.name)
-                        print("Process executions: " + str(current.executions))
-                        print("Process CPU time: " + str(current.counter))
-                        print("Process Waiting: " + str(current.waiting))
                     else:
-                        print("Override current? = " + str(arrived_processes[0].counter < current.counter))
                         top = arrived_processes[0]
-                        print("Top process name: " + top.name)
-                        print("Top process executions: " + str(top.executions))
-                        print("Top process CPU time: " + str(top.counter))
-                        print("Top process Waiting: " + str(top.waiting))
                         if arrived_processes[0].counter < current.counter:
                             if current not in arrived_processes:
                                 arrived_processes.append(current)
 
-                            print("Prev process name: " + current.name)
-                            print("Prev process executions: " + str(current.executions))
-                            print("Prev process CPU time: " + str(current.counter))
-                            print("Prev process Waiting: " + str(current.waiting))
                             current = arrived_processes.pop(0)
                             self.job_order.append([current.name, current_time, current_time + 1])
                             current.waiting = (current_time - (current.executions)) - current.arrival
@@ -175,21 +153,12 @@ class Algorithm(object):
                             current.waiting = (current_time - (current.executions)) - current.arrival
                             current.executions += 1
                             current.decrease_burst(factor=1)
-                            print("Process name: " + current.name)
-                            print("Process executions: " + str(current.executions))
-                            print("Process CPU time: " + str(current.counter))
-                            print("Process Waiting: " + str(current.waiting))
                 else:
                     self.job_order.append([current.name, current_time, current_time + 1])
                     current.waiting = (current_time - (current.executions)) - current.arrival
                     current.executions += 1
                     current.decrease_burst(factor=1)
-                    print("Process name: " + current.name)
-                    print("Process executions: " + str(current.executions))
-                    print("Process CPU time: " + str(current.counter))
-                    print("Process Waiting: " + str(current.waiting))
-
-                print("\n")
+                    
             current_time += 1
 
         total_waiting = 0
